9e9e09153588a47faf75b35d20d6bbf3035dd1a5
Minh898989 <minhhh270805@gmail.com>
2025-05-14 13:02:46 +0700

    p

--- src/components/NewGameScreen.jsx
+++ src/components/NewGameScreen.jsx
@@ -1,4 +1,4 @@
-import { Chess } from "chess.js";
+import Chess from "chess.js";
 import { Chessboard } from "react-chessboard";
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
